778ff59a2cca92ce245328a9a52fe764c626cb71
junlun26 <142468461+junlun26@users.noreply.github.com>
2023-08-17 12:11:52 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+
+app = Flask(__name__)  
+line_bot_api = LineBotApi('U5hlah8NxPTDI61P6+cqf+sVBTJixlenSE/ihCGVYVRf0kbVGUMP57qOBpxxzTIIeXdWvX5dnF6jXdOP/MASdmTKiV/XM5qbRVUHn0DvXckVqNVTBo/A+zT+v+NHTr7li4fjHgwFT8JgvFBDVYZjOAdB04 t89/1O/w1cDnyilFU=')  
+handler = WebhookHandler('1b59f7e2aae7872e0ec20799920c474b')
+
+@app.route("/callback", methods=['POST']) 
+def callback():     
+    # get X-Line-Signature header value     
+    signature = request.headers['X-Line-Signature']
+    # get request body as text     
+    body = request.get_data(as_text=True)     
+    app.logger.info("Request body: " + body)      
+    # handle webhook body     
+    try:         
+        handler.handle(body, signature)     
+    except InvalidSignatureError:         
+        abort(400)      
+    return 'OK'
