c155e7442285d86254f01a902d2b49a2ed426cf0
junlun26 <142468461+junlun26@users.noreply.github.com>
2023-08-19 15:58:12 +0800

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('U5hlah8NxPTDI61P6+cqf+sVBTJixlenSE/ihCGVYVRf0kbVGUMP57qOBpxxzTIIeXdWvX5dnF6jXdOP/MASdmTKiV/XM5qbRVUHn0DvXckVqNVTBo/A+zT+v+NHTr7li4fjHgwFT8JgvFBDVYZjOAdB04 t89/1O/w1cDnyilFU=')  
 handler = WebhookHandler('1b59f7e2aae7872e0ec20799920c474b')
 
-#line_bot_api.push_message('你自己的ID', TextSendMessage(text='你可以開始了'))
+line_bot_api.push_message('你自己的ID', TextSendMessage(text='你可以開始了'))
 
 @app.route("/callback", methods=['POST']) 
 def callback():     
