0963ee112379f4de45485639cb36cbc372c6d708
junlun26 <142468461+junlun26@users.noreply.github.com>
2025-04-07 23:21:54 +0800

    app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
+#import requests
+#from bs4 import BeautifulSoup
+#import pandas as pd
 import pygsheets
 import json
 
